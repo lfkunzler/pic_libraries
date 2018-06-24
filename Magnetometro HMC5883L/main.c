726871c08040b1dd57a6b8b5b708c3c47f6c8a76
Luis Felipe Kunzler <kunzler.luis@gmail.com>
2018-06-24 22:13:15 +0900

    Correcao no magnetometro
    Apresenta as coordenadas X, Y e Z no display.

--- Magnetometro HMC5883L/main.c
+++ Magnetometro HMC5883L/main.c
@@ -1,69 +1,69 @@
 #define _XTAL_FREQ 20000000     // Usar cristal de 20MHz 
+
+#include <xc.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>										/* Include math header file */
-#include "biblioteca.h"
+#include <string.h>
+
 #include "I2C_Master_File.h"
-#include "mylcd.h"
+#include "display.h"
+#include "main.h"
 
-
-
-#define PI	3.14159265359								/* Define Pi value */
-#define Declination	-0.301651						   /* declinio magnetico em caxias do sul, http://www.magnetic-declination.com */
-
-void Magneto_init()										/* Magneto initialize function */
+void magneto_init() /* Magneto initialize function */
 {
-	I2C_Start(0x3C);									/* Start and write SLA+W */
-	I2C_Write(0x00);									/* Write memory location address */
-	I2C_Write(0x70);									/* Configure register A as 8-average, 15 Hz default, normal measurement */
-	I2C_Write(0xA0);									/* Configure register B for gain */
-	I2C_Write(0x00);									/* Configure continuous measurement mode in mode register */
-	I2C_Stop();											/* Stop I2C */
+    I2C_Start(0x3C); /* Start and write SLA+W */
+    I2C_Write(0x00); /* Write memory location address */
+    I2C_Write(0x70); /* Configure register A as 8-average, 15 Hz default, normal measurement */
+    I2C_Write(0xA0); /* Configure register B for gain */
+    I2C_Write(0x00); /* Configure continuous measurement mode in mode register */
+    I2C_Stop(); /* Stop I2C */
 }
 
-int Magneto_GetHeading()
+void get_cord(coordenadas_t *coord)
 {
-	int x, y, z;
-	double Heading;
-	I2C_Start_Wait(0x3C);								/* Start and wait for acknowledgment */
-	I2C_Write(0x03);									/* Write memory location address */
-	I2C_Repeated_Start(0x3D);							/* Generate repeat start condition with SLA+R */
-	/* Read 16 bit x,y,z value (2?s complement form) */
-	x = (((int)I2C_Read(0)<<8) | (int)I2C_Read(0));
-	z = (((int)I2C_Read(0)<<8) | (int)I2C_Read(0));
-	y = (((int)I2C_Read(0)<<8) | (int)I2C_Read(1));
-	I2C_Stop();											/* Stop I2C */
-	Heading = atan2((double)y,(double)x) + Declination;
-	if (Heading>2*PI)									/* Due to declination check for >360 degree */
-		Heading = Heading - 2*PI;
-	if (Heading<0)										/* Check for sign */
-		Heading = Heading + 2*PI;
-	return ((int)(Heading* 180 / PI));					/* Convert into angle and return */
+    I2C_Start_Wait(0x3C); /* Start and wait for acknowledgment */
+    I2C_Write(0x03); /* Write memory location address */
+    I2C_Repeated_Start(0x3D); /* Generate repeat start condition with SLA+R */
+    /* Read 16 bit x,y,z value (2?s complement form) */
+    coord->x = (((int) I2C_Read(0) << 8) | (int) I2C_Read(0));
+    coord->z = (((int) I2C_Read(0) << 8) | (int) I2C_Read(0));
+    coord->y = (((int) I2C_Read(0) << 8) | (int) I2C_Read(1));
+    I2C_Stop(); /* Stop I2C */
 }
-
 
 int main(void)
 {
     ADCON1 = 0x0F;
-	char buffer[25];
-	//TRISB &= ~(1<<4);									/* Make pin5 of PORT B as input */	
-    TRISA = 0x00; // porra toda como saida
+    char buffer[17];
+    memset(buffer, 0x20, 17);
+
+    TRISA = 0x00;
+
     RA0 = 1;
-  // Inicializa LCD
-    inic_XLCD();
+
+    lcd_t lcd;
+    coordenadas_t cord;
+
+    // Inicializa LCD
+    lcd_init(&lcd);
+
     while (BusyXLCD());
-	I2C_Init();											/* Initialize I2C */
-	Magneto_init();										/* Initialize magneto */
-	SetDDRamAddr(0x01);
-   while (BusyXLCD());
-    putsXLCD("MAGNETOMETRO\0");
-	while (1)
-	{
-        RA0 = ! RA0;
-        Mydelay(500);        
-        sprintf(buffer,"Heading = %d%c    ",Magneto_GetHeading(),0xDF);
-         SetDDRamAddr(0x40);
-            while (BusyXLCD());
-            putsXLCD(buffer);
-	}
+    I2C_Init(); /* Initialize I2C */
+    magneto_init(); /* Initialize magneto */
+    lcd_write(&lcd, 1, 1, "  X    Y    Z  \0");
+    
+    while (1) {
+        get_cord(&cord);
+        
+        sprintf(buffer, "%4d %4d %4d\0", cord.x, cord.y, cord.z);
+        lcd_write(&lcd, 2, 1, buffer);
+        lcd_runtime(&lcd);
+    }
+}
+
+void delay_ms(uint16_t milis)
+{
+    for (uint16_t t = 0; t < milis; t++) {
+        __delay_ms(1);
+    }
 }
