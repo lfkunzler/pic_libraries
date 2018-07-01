3790a5d30d831721a7ace085d38a8a3ed1c4770a
Luis Felipe Kunzler <kunzler.luis@gmail.com>
2018-07-01 17:37:20 +0900

    Correcao do timer, inclusao do arquivo do proteus
    teste da lcd no portd

---
+++ GigaDeTestes.X/display.c
@@ -0,0 +1,114 @@
+/*
+ * File:   display.c
+ * Authors: Luis Felipe Kunzler & Ruan Carlos Pinto
+ *
+ * Created on 18 de Maio de 2018, 01:36
+ */
+
+#include <xc.h>
+#include <stdint.h>
+
+#include "xlcd.h"
+#include "display.h"
+
+// Inicializa o LCD
+
+void lcdInit(lcd_t *lcd) {
+    TRISD = 0;
+    
+    OpenXLCD(FOUR_BIT & LINES_5X7); // configura para 4-bit, 5x7, multiplas linhas
+    while (BusyXLCD());
+    WriteCmdXLCD(0x06); // move o cursor para direita depois de escrever 
+    while (BusyXLCD());
+    WriteCmdXLCD(0x0C); // display ligado sem aparecer o cursor
+    while (BusyXLCD());
+
+    lcdCleanAll(lcd);
+}
+
+// Limpa a tela do display
+void lcdClean(void) {
+    while (BusyXLCD());
+    WriteCmdXLCD(0x01); // comando para limpar a tela do display
+
+    delay_ms(80); // Eh necessario dar um tempo depois de limpar
+}
+
+// limpa a memoria, inserindo espacos
+void lcdMemClean(lcd_t *lcd) {
+    for (uint8_t i = 0; i < 16; i++) {
+        lcd->Memory[0][i] = 0x20;
+        lcd->Memory[1][i] = 0x20;
+    }
+    lcd->NeedsRedraw = 1;
+}
+
+void lcdCleanAll(lcd_t *lcd) {
+    lcdClean();
+    lcdMemClean(lcd);
+}
+
+
+int lcdWrite(lcd_t *lcd, uint8_t row, uint8_t col, char *string) {
+    // se estiver fora dos limites de escrita
+    if (row == 0 || row > 2 || col == 0 || col > 16) {
+        return -1;
+    }
+
+    row--; // ajusta para posicao de vetor
+    col--;
+
+    if (row == 0) { // se for escrever na linha de cima
+        while (*string && col <= 0x0F) {
+            lcd->Memory[0][col++] = *string;
+            string++;
+        }
+        col = 0; // volta para a primeira posicao
+        row = 1; // da linha de baixo
+    }
+
+    // se ainda houver dados ou se a instrucao for apenas para a linha de baixo
+    while (*string && col <= 0x0F) {
+        lcd->Memory[1][col++] = *string;
+        string++;
+    }
+
+    lcd->NeedsRedraw = 1;
+
+    return 0;
+}
+
+void lcdRuntime(lcd_t *lcd) {
+    while (BusyXLCD());
+
+    if (lcd->NeedsRedraw == 1) {
+        SetDDRamAddr(0x00);
+        for (int i = 0; i < 16; i++) {
+            while (BusyXLCD());
+            putcXLCD((char) lcd->Memory[0][i]);
+        }
+        SetDDRamAddr(0x40);
+        for (int i = 0; i < 16; i++) {
+            while (BusyXLCD());
+            putcXLCD((char) lcd->Memory[1][i]);
+        }
+
+        lcd->NeedsRedraw = 0;
+    }
+}
+
+//////////////// Funcoes de tempo para a xlcd.h ///////////
+
+void DelayFor18TCY(void) {
+    for (int i = 0; i < 18; i++) {
+        continue;
+    }
+}
+
+void DelayPORXLCD(void) {
+    delay_ms(60);
+}
+
+void DelayXLCD(void) {
+    delay_ms(20);
+}
--- GigaDeTestes.X/main.c
+++ GigaDeTestes.X/main.c
@@ -1,45 +1,52 @@
 #include <xc.h>
 
 #include "main.h"
+#include "xlcd.h"
+#include "display.h"
 
 #define LED_TESTE PORTCbits.RC0
 
+volatile lcd_t lcd;
+
+
 void interrupt ISR()
 {
-    
+
 }
 
 void interrupt low_priority ISR_LP()
 {
     if (TMR1IF) {
-        TMR1IF = 0; // desliga interrupcao
-        LED_TESTE != LED_TESTE;
+        TMR1IF = 0; // desliga interrupcao        
+        TMR1H = 0x63; // configuracao para uma interrupcao a cada 20ms
+        TMR1L = 0xC0;
+
+        // TODO: rotinas que rodam a cada 20ms
+        lcd.NeedsRedraw = 1;
     }
 }
 
-
-/* EQUACAO PARA CALCULAR O TEMPO DE INTERRUPCAO DO TMR1*/
-// t = Tosc * prescaler * (2^16 - (TMR1H:TMR1L))
-// 20ms = 0.02s = 4/8000000 * PRES * (2^16 - (TMR1H:TMR1L))
-// (0.02 * 8000000/4) / PRES = 2^16 - (TMR1H:TMR1L)
-//  400000/1 = 65536 - regs
-// regs = 65536 - 40000
-// regs = 25536 = 0x63C0
 void main(void)
 {
     ADCON1 = 0x0F;
     OSCCON = 0x72; // osc interno a 8MHz
-    
+
     TRISC = 0xFE; // RC0 em output
     LED_TESTE = 1;
-    
+
     /* CONFIGURACAO DAS INTERRUPCOES */
     IPEN = 1; // ativa diferenca de prioridade entre interrupcoes
-    //GIEH = 1; // ativa interrupcoes globais de alta prioridade
-    //GIEL = 1; // ativa interrupcoes globais de baixa prioridade
-    PEIE = 1; // ativa interrupcos de baixa prioridade
-    
+    GIEH = 1; // ativa interrupcoes alta prioridade
+    PEIE = 1; // ativa interrupcos perifericas de baixa prioridade
+
     /* CONFIGURACAO DO TIMER 1 */
+    /* EQUACAO PARA CALCULAR O TEMPO DE INTERRUPCAO DO TMR1*/
+    // t = Tosc * prescaler * (2^16 - (TMR1H:TMR1L))
+    // 20ms = 0.02s = 4/8000000 * PRES * (2^16 - (TMR1H:TMR1L))
+    // (0.02 * 8000000/4) / PRES = 2^16 - (TMR1H:TMR1L)
+    //  400000/1 = 65536 - regs
+    // regs = 65536 - 40000
+    // regs = 25536 = 0x63C0
     T1CONbits.RD16 = 1; // usa 16 bits
     T1CONbits.T1RUN = 0; // nao usa o clock auxiliar
     T1CONbits.T1CKPS = 0; // pre em 1
@@ -47,12 +54,17 @@ void main(void)
     T1CONbits.TMR1CS = 0; // osc e baseado no ciclo de maquina (fosc/4)
     T1CONbits.TMR1ON = 1; // tmr1 ativo     
     TMR1H = 0x63; // configuracao para uma interrupcao a cada 20ms
-    TMR1L = 0xC0;    
+    TMR1L = 0xC0;
     TMR1IF = 0; // desliga interrupcoes existentes
     TMR1IE = 1; // ativa as interrupcoes do timer 1
     TMR1IP = 0; // timer1 peripheral interrupt em baixa prioridade
+
+    /* UTILIZACAO DO DISPLAY */
+    lcdInit(&lcd);
+    lcdWrite(&lcd, 1, 1, "pau no cu do        patric\0");
+    
     
     while (1);
-    
+
     return;
 }
